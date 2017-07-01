09a91e726b4ef5e02dcb62982d7ece1a642468f8
Jacob Garby <j.garby@icloud.com>
2017-07-01 12:54:44 +0100

    Changed markdown a bit and done loooaddds of css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sign in</title>
+    @component('styles')
+    @endcomponent
+    <link rel="stylesheet" href="{!! asset('css/auth.css') !!}">
+  </head>
+  <body>
+    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+      {{ csrf_field() }}
 
-<form class="form-horizontal" method="POST" action="{{ route('login') }}">
-{{ csrf_field() }}
+      <span class="title">cordial</span>
 
-<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+      <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
 
-    <div class="col-md-6">
-        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+        <div class="email">
+          <input id="email" type="email" placeholder="Email address" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-        @if ($errors->has('email'))
+          @if ($errors->has('email'))
             <span class="help-block">
-                <strong>{{ $errors->first('email') }}</strong>
+              <strong>{{ $errors->first('email') }}</strong>
             </span>
-        @endif
-    </div>
-</div>
-
-<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-    <label for="password" class="col-md-4 control-label">Password</label>
-
-    <div class="col-md-6">
-        <input id="password" type="password" class="form-control" name="password" required>
-
-        @if ($errors->has('password'))
-            <span class="help-block">
-                <strong>{{ $errors->first('password') }}</strong>
-            </span>
-        @endif
-    </div>
-</div>
-
-<div class="form-group">
-    <div class="col-md-6 col-md-offset-4">
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-            </label>
+          @endif
         </div>
-    </div>
-</div>
+      </div>
 
-<div class="form-group">
-    <div class="col-md-8 col-md-offset-4">
-        <button type="submit" class="btn btn-primary">
+      <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+
+        <div class="password-wrap">
+          <input id="password" placeholder="Password" type="password" class="form-control" name="password" required>
+
+          @if ($errors->has('password'))
+            <span class="help-block">
+              <strong>{{ $errors->first('password') }}</strong>
+            </span>
+          @endif
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="remember-wrap">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Stay logged in?
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="submit-wrap">
+          <button type="submit" class="button-big close-right">
             Login
-        </button>
-    </div>
-</div>
-</form>
+          </button>
+        </div>
+      </div>
+    </form>
+  </body>
+</html>
